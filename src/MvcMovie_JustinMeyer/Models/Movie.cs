2346e44e1d4a60023da492edccb47b81820dd18f
jmeyer431 <justin.meyer@ingeniux.com>
2016-08-26 10:55:30 -0700

    Completed Adding Validation

--- src/MvcMovie_JustinMeyer/Models/Movie.cs
+++ src/MvcMovie_JustinMeyer/Models/Movie.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie_JustinMeyer.Models
 {
-    public class Movie
+    public class Movie 
     {
         public int ID
         {
@@ -11,38 +12,41 @@ namespace MvcMovie_JustinMeyer.Models
             set;
         }
 
+        [StringLength(60, MinimumLength = 3)]
         public string Title
         {
             get;
             set;
         }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate
         {
             get;
             set;
         }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), Required, StringLength(30)]
         public string Genre
         {
             get;
             set;
         }
 
+        [Range(1, 100), DataType(DataType.Currency)]
         public decimal Price
         {
             get;
             set;
         }
-        
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), StringLength(5)]
         public string Rating
         {
             get;
             set;
         }
 
-        
+       
     }
 }
